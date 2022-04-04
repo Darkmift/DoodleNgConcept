165b0edfc448f03ea217cbe1077a0c920941f20f
darkmift <darkmift@gmail.com>
2022-04-04 20:54:19 +0300

    applied local config - no fetch!

---
+++ src/assets/loadConfig.js
@@ -0,0 +1,5 @@
+import configs from './config.json';
+
+window.configGlobal = configs;
+
+console.log("🚀 ~ file: loadConfig.js ~ line 2 ~ configs", configs)
--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,19 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
 declare global {
   interface Window {
     configGlobal: any;
   }
 }
 
-fetch('./assets/config.json')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('🚀 ~ file: main.ts ~ line 10 ~ data', data);
-    window.configGlobal = data;
-    
-    if (environment.production) {
-      enableProdMode();
-    }
+console.log('🚀 ~ file: main.ts ~ line 10 ~ data', window.configGlobal);
 
-    platformBrowserDynamic()
-      .bootstrapModule(AppModule)
-      .catch((err) => console.error(err));
-  })
-  .catch((error) => console.log(error));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));

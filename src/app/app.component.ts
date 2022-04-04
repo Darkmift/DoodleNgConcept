c031a4417d54a7e5b7e0974e2c6d6614164fe3cb
darkmift <darkmift@gmail.com>
2022-04-04 20:34:18 +0300

    a sample global declaration w/ async load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+  ngOnInit() {
+    console.log(
+      '🚀 ~ file: app.component.ts ~ line 13 ~ AppComponent ~ ngOnInit ~ globalConfig',
+      window.configGlobal
+    );
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
+declare global {
+  interface Window {
+    configGlobal: any;
+  }
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+fetch('./assets/config.json')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('🚀 ~ file: main.ts ~ line 10 ~ data', data);
+    window.configGlobal = data;
+    
+    if (environment.production) {
+      enableProdMode();
+    }
+
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .catch((err) => console.error(err));
+  })
+  .catch((error) => console.log(error));
